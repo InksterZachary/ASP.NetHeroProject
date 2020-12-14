5ef9fe919570c7abbf8b74feb7b335c3da8a57a6
Zachary Inkster <z.j.inkster@gmail.com>
2020-12-14 10:13:24 -0600

    Added migration, view, and controller for Hero

--- Superhero/Data/ApplicationDbContext.cs
+++ Superhero/Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Superhero.Models;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -8,9 +9,12 @@ namespace Superhero.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public DbSet<Hero> Heroes { get; set; }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
         }
+
+        
     }
 }
--- Superhero/Models/Hero.cs
+++ Superhero/Models/Hero.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,12 @@ namespace Superhero.Models
 {
     public class Hero
     {
+        [Key]
+        public int Id { get; set; }
+        public string name { get; set; }
+        public string alterEgo { get; set; }
+        public string mainPower { get; set; }
+        public string lesserPower { get; set; }
+        public string catchPhrase { get; set; }
     }
 }
