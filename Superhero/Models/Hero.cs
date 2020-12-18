4cec7232055d75e4ed832e4a71b67b05b194e0de
Zachary Inkster <z.j.inkster@gmail.com>
2020-12-17 21:23:02 -0600

    Updated display names for Hero.cs variables

--- Superhero/Models/Hero.cs
+++ Superhero/Models/Hero.cs
@@ -10,10 +10,15 @@ namespace Superhero.Models
     {
         [Key]
         public int Id { get; set; }
+        [Display(Name ="Super Name")]
         public string name { get; set; }
+        [Display(Name ="Hidden Identity")]
         public string alterEgo { get; set; }
+        [Display(Name ="Main Power")]
         public string mainPower { get; set; }
+        [Display(Name ="Secondary Power")]
         public string lesserPower { get; set; }
+        [Display(Name ="Catchphrase")]
         public string catchPhrase { get; set; }
     }
 }
